4e7dc867d9ce4b5adebde8873f556d817f53163c
hkuniyoshi-cpu <h.kuniyoshi@search-mania.net>
2026-08-26 14:46:35 +0900

    sitemap.xml.js: drop function-level edge cache to avoid stale response

--- functions/sitemap.xml.js
+++ functions/sitemap.xml.js
@@ -3,24 +3,17 @@
 
 const GAS_URL = 'https://script.google.com/macros/s/AKfycbyu21Op12Ty_Q_F4rZUWonLQ9xefD_O91rmyavRhZJB70GRWGS68_iwMqx-xWAmj-dwYg/exec';
 
-export async function onRequest(context) {
-  const cache = caches.default;
-  const cacheKey = new Request('https://sfp-okinawa.search-mania.net/sitemap.xml', context.request);
-  let response = await cache.match(cacheKey);
-  if (response) return response;
-
+export async function onRequest() {
   try {
     const upstream = await fetch(GAS_URL + '?sitemap=1', { redirect: 'follow' });
     const xml = await upstream.text();
-    response = new Response(xml, {
+    return new Response(xml, {
       status: 200,
       headers: {
         'Content-Type': 'application/xml; charset=utf-8',
         'Cache-Control': 'public, max-age=3600, s-maxage=3600',
       },
     });
-    context.waitUntil(cache.put(cacheKey, response.clone()));
-    return response;
   } catch (e) {
     return new Response('<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"><url><loc>https://sfp-okinawa.search-mania.net/</loc></url></urlset>', {
       status: 200,
